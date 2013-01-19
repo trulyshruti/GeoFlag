5e0321bdfb0323eb8cfa1e3029319ab267250960
Jim Chen <knh.jabbany@gmail.com>
2013-01-19 06:06:35 -0500

    Created better gui and handles file choosing

--- iface/geotag.js
+++ iface/geotag.js
@@ -1,10 +1,31 @@
+var Main = {
+	fileSelection:{}
+}
+function smoothScrollTo(e){
+	var elm = document.getElementById(e);
+	if(elm == null) return;
+	var xnew = Math.ceil(elm.offsetTop - (elm.offsetTop - window.pageYOffset) / 2);
+	window.scrollTo(0, xnew);
+	if(Math.abs(xnew - elm.offsetTop) -1){
+		setTimeout(function(){smoothScrollTo(e);},80);
+	}
+}
 window.addEventListener("load",function(){
 	filepicker.setKey(KEY_FILEPICKER);
-	$("#pickerbtn")[0].addEventListener("click",function(){
-		filepicker.pick({
-			
-		},function(g){
-			
+	window.scrollTo(0,0);
+	$("#startbtn").bind("click",function(){
+		$("#section2").css("display","");
+		smoothScrollTo("section2");
+	});
+	$("#addflagbtn").bind("click",function(){
+		$("#addModal").modal();
+	});
+	$("#pickerbtn").bind("click",function(){
+		filepicker.pick({},function(g){
+			$("#section3").css("display","");
+			smoothScrollTo("section3");
+			$("#fileselection").text("You've selected \"" + g.filename + "\"!");
+			fileSelection = g;
 		},function(h){
 			
 		});
