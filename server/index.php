91e2d74e850db9125eab5c9a33e767990bf499e5
Derek Xingzhou He <derekhe+git@gmail.com>
2013-01-19 18:12:40 -0500

    API finished

--- server/index.php
+++ server/index.php
@@ -3,6 +3,21 @@
 ini_set('display_errors', true);
 error_reporting(E_ALL);
 
+  function distance($a, $b, $x, $y)
+  {
+  	$R = 6371; // Radius of the earth in km
+  	$dLat = deg2rad($x-$a);  // deg2rad below
+  	$dLon = deg2rad($y-$b); 
+  	$a = 
+    		sin($dLat/2) * sin($dLat/2) +
+    		cos(deg2rad($a)) * cos(deg2rad($x)) * 
+    		sin($dLon/2) * sin($dLon/2)
+    		; 
+  	$c = 2 * atan2(sqrt($a), sqrt(1-$a)); 
+  	$d = $R * $c; // Distance in km
+  	return ($d) < 0.05;
+  }
+
   // Facebook
   require_once("facebook-php-sdk/src/facebook.php");
 
@@ -22,22 +37,53 @@ error_reporting(E_ALL);
   $mongoDB = new MongoClient();
   $database = $mongoDB -> geoflag;
   $userLink = $database -> userLink;
-  $file = $database -> file;
+  $files = $database -> files;
   $opened = $database -> opened;
 
   switch ($_POST['action'])
   {
      case 'add': 
+                 $newFile = array();
+                 $newFile['owner'] = $uid;
+                 $newFile['fileName'] = $_POST['fileName'];
+                 $newFile['url'] = $_POST['url'];
+                 $newFile['id'] = time();
+		 $newFile['rule'] = array();
+                 $newFile['rule'][0] = array();
+                 $newFile['rule'][0]['lat'] = $_POST['lat'];
+                 $newFfile['rule'][0]['lng'] = $_POST['lng'];
+                 if (isset($_POST['startTime']))
+                    $newFile['rule'][0]['startTime'] = new MongoDate(strtotime($_POST['startTime']));
+                 if (isset($_POST['endTime']))
+                    $newFile['rule'][0]['endTime'] = new MongoDate(strtotime($_POST['endTime']));
+                 
+                 $files->insert($file);
+
 		 echo "{'status':200}";
                  break;
      case 'check':
-                 /*
-                 $cursor = $userLink -> find(array("from" => $uid));
-                 foreach ( $cursor as $linkId => $link )
+                 
+		 $lat = $_POST['lat'];
+		 $lng = $_POST['lng'];
+		 
+		 $ans = array();
+
+                 $iter = $userLink -> find(array("from" => $uid));
+                 foreach ( $iter as $linkId => $link )
                  {
-                       $cursor2 = $file -> find(array("owner" => $link['to']));
-                       foreach ( $cursor2 as $fileId => $file )
-                 }*/
+                       $jter = $files -> find(array("owner" => $link["to"]));
+                       foreach ( $jter as $fileId => $file )
+                       {
+				if ($opened -> findone(array("user" => $uid, "id" => $file['id'])) == NULL)
+				{
+					if (distance($file['rule'][0]['lat'], $file['rule'][0]['lng'], $lat, $lng))
+						if (!isset($file['rule'][0]['startTime']) || $file['rule'][0]['startTime']<time())
+							if (!isset($file['rule'][0]['endTime']) || $file['rule'][0]['endTime']>time())
+								$ans[] = $file;
+				}
+                       }
+                 }
+		 echo json_encode($ans);
                  break;
      case 'nuts':
                  die("{'status':404}");
