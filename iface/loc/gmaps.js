a150f4332fa7fc2ec69eba00a96ae6047a4d1f14
Shruti Kulkarni <sgk2118@columbia.edu>
2013-01-19 16:30:20 -0500

    gmaps markers

--- iface/loc/gmaps.js
+++ iface/loc/gmaps.js
@@ -15,5 +15,10 @@ var mapOptions = {
     zoom: 12,
     mapTypeId: google.maps.MapTypeId.ROADMAP
 };
+var map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
-new google.maps.Map(document.getElementById('map'), mapOptions);
+var markerOptions = {
+    position: new google.maps.LatLng(37.7831, -122.4039)
+};
+var marker = new google.maps.Marker(markerOptions);
+marker.setMap(map);
