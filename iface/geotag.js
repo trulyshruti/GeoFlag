d40020d5abf35adccf4f34fdeabbe0e6da6f4b75
Jim Chen <knh.jabbany@gmail.com>
2013-01-19 03:53:33 -0500

    added logic and gitignore

--- iface/geotag.js
+++ iface/geotag.js
@@ -1,3 +1,12 @@
 window.addEventListener("load",function(){
 	filepicker.setKey("AQUNy9UdwSkK8rS4eponPz");
+	$("#pickerbtn")[0].addEventListener("click",function(){
+		filepicker.pick({
+			
+		},function(g){
+			
+		},function(h){
+			
+		});
+	});
 });
