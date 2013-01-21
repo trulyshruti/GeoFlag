4fbb9ba953c38162212ec8455a5961f8da921ac2
Jim Chen <knh.jabbany@gmail.com>
2013-01-21 18:14:26 -0500

    Added sample credentials file

---
+++ credentials.sample.js
@@ -0,0 +1,10 @@
+/***
+Please replace KEY_FILEPICKER with your filepicker App Key, APPID_FACEBOOK with your facebook application id
+and APPBRIDGE_FACEBOOK {YOUR_URL} with the absolute URL address of your server
+
+Then rename this file to credentials.js!!
+***/
+var KEY_FILEPICKER = "";
+var KEY_FACEBOOK = "";
+var APPID_FACEBOOK = "";
+var APPBRIDGE_FACEBOOK = "//{YOUR_URL}/channel.htm";
